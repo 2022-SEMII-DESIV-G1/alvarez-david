5d679cd47463cc58643ab7a58ec78a5fe0eb1eb0
David Alvarez <dddavid.ddaniel08@gmail.com>
2022-10-10 14:51:19 -0500

    haciendo pruebas en js

---
+++ lab8/logica.js
@@ -0,0 +1,13 @@
+const a = "hola mundo";
+
+function repetida(texto){
+    var repetida = 0;
+    var letras = [];
+    var almacenar = [];
+
+    texto = texto.toUpperCase().split("");
+
+    console.log(texto);
+}
+
+repetida(a);
