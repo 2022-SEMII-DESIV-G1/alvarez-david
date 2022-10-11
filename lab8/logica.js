f71aefc5f4470b8d5d0b9300b32b0313a36d39b5
David Alvarez <dddavid.ddaniel08@gmail.com>
2022-10-10 20:53:34 -0500

    borrando lineas innecesarias

--- lab8/logica.js
+++ lab8/logica.js
@@ -21,23 +21,6 @@ function repetida(){
 
     console.log(almacenar);
     console.log(letras);
-    //console.log(texto);
-
-    var valorMaximo = Math.max(...almacenar);
-    //console.log(valorMaximo);
-
-    var letrasRepetidas = "";
-
-    for (let i = 0; i < almacenar.length; i++) {
-        if(almacenar[i] === valorMaximo){
-            letrasRepetidas = letras[i];
-        }
-        
-    }
-
-    return letrasRepetidas + " = " + valorMaximo;
-
-
     
 }
 
