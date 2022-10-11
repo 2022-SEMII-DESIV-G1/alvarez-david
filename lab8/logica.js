b739dd847e88cd0f6b65d1f3fbda3b8b07900a94
David Alvarez <dddavid.ddaniel08@gmail.com>
2022-10-10 19:36:13 -0500

    borrando y modificando todo el js

--- lab8/logica.js
+++ lab8/logica.js
@@ -1,43 +1,6 @@
-const a = "aaaiiiaabb";
-
-function repetida(texto){
-    var repetida = 1;
-    var letras = [];
-    var almacenar = [];
-
-    texto = texto.toUpperCase().replace(/ /g, "").split("").sort();
-
-    for(let i = 0; i < texto.length; i++){
-            if(texto[i + 1] === texto[i] ){
-                repetida ++;
-            }else{
-                letras.push(texto[i]);
-                almacenar.push(repetida);
-                repetida = 1;
-            }
-    }
-
-    console.log(almacenar);
-    console.log(letras);
-    //console.log(texto);
-
-    var valorMaximo = Math.max(...almacenar);
-    //console.log(valorMaximo);
-
-    var letrasRepetidas = "";
-
-    for (let i = 0; i < almacenar.length; i++) {
-        if(almacenar[i] === valorMaximo){
-            letrasRepetidas = letras[i];
-        }
-        
-    }
-
-    return letrasRepetidas + " = " + valorMaximo;
 
 
+function calcular(){
+    var input = document.getElementById("cadenita").value;
     
 }
-
-const resultado = repetida(a);
-console.log(resultado);
