fe25150969b4799b11ab64efdf2ec8b1fc4225b1
David Alvarez <dddavid.ddaniel08@gmail.com>
2022-10-20 10:53:34 -0500

    haciendo pruebas pero no me gusta JAJAJA

--- lab9/logica.js
+++ lab9/logica.js
@@ -1,22 +1,84 @@
-let arreglo1 = [1];
-let arreglo2 = [2, 3]; 
-let arreglo3 = [4, 5, 6];
-let arreglo4 = [7, 8, 9, 10];
+let arreglo1 = [75];
+let arreglo2 = [95, 64]; 
+let arreglo3 = [17, 47, 82];
+let arreglo4 = [18, 35, 87, 10];
+let arreglo5 = [20, 04, 82, 47, 65];
+let arreglo6 = [19, 01, 23, 75, 03, 34];
+let arreglo7 = [88, 02, 77, 73, 07, 63, 67];
+let arreglo8 = [99, 65, 04, 28, 06, 16, 70, 92];
+let arreglo9 = [41, 41, 26, 56, 83, 40, 80, 70, 33];
+let arreglo10 = [41, 48, 72, 33, 47, 32, 37, 16, 94, 29];
+let arreglo11 = [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14];
+let arreglo12 = [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57];
+let arreglo13 = [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48];
+let arreglo14 = [63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31];
+let arreglo15 = [04, 62, 98, 27, 23, 09, 70, 98, 73, 93, 38, 53, 60, 04, 23];
 
-let arreglo5 = [arreglo1, arreglo2, arreglo3, arreglo4];
+let arreglof = [arreglo1, arreglo2, arreglo3, arreglo4, arreglo5, arreglo6, arreglo7, arreglo8, arreglo9, arreglo10, arreglo11, 
+                arreglo12, arreglo13, arreglo14, arreglo15];
 
 
 //document.write(arreglo5);
 
 //document.write("<p>" + arreglo5 + "</p>");
 
-for(var i = 0; i<arreglo5.length; i++){
+for(var i = 0; i<arreglof.length; i++){
+        if(i === 0){
+            console.log("\t\t\t\t\t\t\t\t\t\t "+arreglof[0]);   
+        }
+        if(i === 1){
+            console.log("                                        "+arreglof[1]); 
+        }
+        if(i === 2){
+            console.log("                                       "+arreglof[2]); 
+        }
+        if(i === 3){
+            console.log("                                      "+arreglof[3]); 
+        }
+        if(i === 4){
+            console.log("                                     "+arreglof[4]); 
+        }
+        if(i === 5){
+            console.log("                                    "+arreglof[5]); 
+        }
+        if(i === 6){
+            console.log("                                   "+arreglof[6]); 
+        }
+        if(i === 7){
+            console.log("                                  "+arreglof[7]); 
+        }
+        if(i === 8){
+            console.log("                                 "+arreglof[8]); 
+        }
+        if(i === 9){
+            console.log("                                "+arreglof[9]); 
+        }
+        if(i === 10){
+            console.log("                               "+arreglof[10]); 
+        }
+        if(i === 11){
+            console.log("                              "+arreglof[11]); 
+        }
+        if(i === 12){
+            console.log("                             "+arreglof[12]); 
+        }
+        if(i === 13){
+            console.log("                            "+arreglof[13]); 
+        }
+        if(i === 14){
+            console.log("                           "+arreglof[14]); 
+        }
 
-        console.log(""+arreglo5[i]);   
+        
 }
 
 
-arreglo5.forEach((l, i) => {
+/*for(var j = 0; j<arreglof.length; j++){
+    console.log(""+arreglof[j]);
+} */
+
+
+/* arreglo5.forEach((l, i) => {
     console.log( ""+arreglo5[i]);
-});
+}); */
 //console.log(arreglo5)
