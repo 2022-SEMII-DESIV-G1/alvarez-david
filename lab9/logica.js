fce9ec39ba6e6eb27e918c117eece32945472f58
David Alvarez <dddavid.ddaniel08@gmail.com>
2022-10-31 18:53:33 -0500

    gg

--- lab9/logica.js
+++ lab9/logica.js
@@ -1,53 +1,49 @@
-let arreglo1 = [75];
-let arreglo2 = [95, 64]; 
-let arreglo3 = [17, 47, 82];
-let arreglo4 = [18, 35, 87, 10];
-let arreglo5 = [20, 04, 82, 47, 65];
-let arreglo6 = [19, 01, 23, 75, 03, 34];
-let arreglo7 = [88, 02, 77, 73, 07, 63, 67];
-let arreglo8 = [99, 65, 04, 28, 06, 16, 70, 92];
-let arreglo9 = [41, 41, 26, 56, 83, 40, 80, 70, 33];
-let arreglo10 = [41, 48, 72, 33, 47, 32, 37, 16, 94, 29];
-let arreglo11 = [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14];
-let arreglo12 = [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57];
-let arreglo13 = [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48];
-let arreglo14 = [63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31];
-let arreglo15 = [04, 62, 98, 27, 23, 09, 70, 98, 73, 93, 38, 53, 60, 04, 23];
-
-let arreglof = [arreglo1, arreglo2, arreglo3, arreglo4, arreglo5, arreglo6, arreglo7, arreglo8, arreglo9, arreglo10, arreglo11, 
-                arreglo12, arreglo13, arreglo14, arreglo15];
-
-let arregloR = [];
+/* let arreglo =[ 
+[75],
+[95, 64], 
+[17, 47, 82],
+[18, 35, 87, 10],
+[20, 04, 82, 47, 65],
+[19, 01, 23, 75, 03, 34],
+[88, 02, 77, 73, 07, 63, 67],
+[99, 65, 04, 28, 06, 16, 70, 92],
+[41, 41, 26, 56, 83, 40, 80, 70, 33],
+[41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+[53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+[70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+[91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+[63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+[04, 62, 98, 27, 23, 09, 70, 98, 73, 93, 38, 53, 60, 04, 23]]; */
 
 
-//document.write(arreglo5);
+let prueba = [ [1],
+[2,3],
+[4,5,6]];
 
-//document.write("<p>" + arreglo5 + "</p>");
+let suma = 0;
+let suma2 = 0;
+let j = 0;
 
-for(var i = 0; i<arreglof.length; i++){    
-    console.log(""+arreglof[i])      
+for(var i = 0; i<prueba.length; i++){    
+    console.log(" "+prueba[i]);
 } 
 
-let contador=0;
-
-for(var i=0; i<15; i++){
-    contador += arreglof[i][0];
+var acum = 0;
+for(var i = 0; i<3; i++){    
+    
+    acum += prueba[i][0];
 }
-console.log(contador);
-arregloR[0] = contador;
-contador = 0; 
 
-for(var i=0; i<15; i++){
-    contador += arreglof[i][i];
-}
-console.log(contador);
-arregloR[1] = contador;
+for(var i = 0; i<3; i++){    
+    
+    suma+= prueba[i][i];
+} 
 
 
 
-//console.log(arregloR);
+console.log(acum);
+console.log(suma);
+console.log(suma2);
 
-/* arreglo5.forEach((l, i) => {
-    console.log( ""+arreglo5[i]);
-}); */
-//console.log(arreglo5)
+//console.log(suma);
+
